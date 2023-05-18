8ca9b95acff30ad2c6ba492acd800e513fe31207
Alberto <74522184+albim360@users.noreply.github.com>
2023-05-18 17:21:06 +0200

    FEAT required added

--- database/migrations/2023_05_18_145013_create_typologies_table.php
+++ database/migrations/2023_05_18_145013_create_typologies_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('typologies', function (Blueprint $table) {
             $table->id();
-            $table->string('category_kitchen')->nullable();
+            $table->string('category_kitchen')->required();
             $table->timestamps();
         });
     }
