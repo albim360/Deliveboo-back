ec60380f50626e0cc763c9206bd9b7cbb58a2024
KrisssBrown <cristianbruno.cs@gmail.com>
2023-05-18 17:25:27 +0200

    FEAT add orders table

--- database/migrations/2023_05_18_145047_create_orders_table.php
+++ database/migrations/2023_05_18_145047_create_orders_table.php
@@ -15,6 +15,12 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->dateTime('date')->required();
+            $table->decimal('total_payment', 10, 2)->required();
+            $table->string('full_name', 50)->required();
+            $table->string('telephone', 30)->required();
+            $table->string('address', 100)->required();
+            $table->string('email')->required();
             $table->timestamps();
         });
     }
