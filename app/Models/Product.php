f6e31532096958b8cc952e4405d0dbaadb260b26
Giovannirighini2000 <righinigiovanni0@gmail.com>
2023-05-19 15:03:02 +0200

    edit migrations-models-seeders

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+    protected $fillable = ['name','price','description'];
+    public function restaurants()
+    {
+        return $this->hasMany(Restaurant::class);
+    }
+    public function orders()
+    {
+        return $this->belongsToMany(Order::class);
+    }
 }
