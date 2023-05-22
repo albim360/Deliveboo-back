1df7b135dd213f9620fefdc3ed78024c6b50ff23
Giovannirighini2000 <righinigiovanni0@gmail.com>
2023-05-22 11:53:07 +0200

    feat added create edit index show

---
+++ resources/views/Restaurants/index.blade.php
@@ -0,0 +1,65 @@
+@exteds('layouts.app')
+
+@section('content')
+<div class="container">
+    <div>
+        <h1>
+            i ristoranti
+        </h1>
+        <div>
+            <a class="btn" href="{{route('restaurants.create')}}">Nuovo progetto</a>
+        </div>
+    </div>
+    
+                @forelse ($restaurants as $restaurant)
+                
+                    <p>{{$project->id}}</p>
+                    <p><a href="{{route('restaurants.show',$restaurant)}}">{{$restaurant->company_name}}</a></p>
+                    <p>{{$restaurant->address}}</p>
+                    <p>{{$restaurant->description}}</p>
+                    <p>{{$restaurant->vat_number}}</p>
+                    <p>{{$restaurant->telephone}}</p>
+                    
+                    <p>
+                        {{ $restaurant->trashed() ? $restaurant->deleted_at : '' }}
+                    </p>
+                    <p> {{ $restaurant->typology ? $restaurant->typology->category_kitchen : '-' }} </p>
+                    <botton>
+                        <a class="btn " href="{{route('restaurants.edit',$restaurant)}}">MODIFICA</a>
+                        
+                    </botton>
+                    <botton>
+                        <form action="{{ route('restaurants.destroy', $restaurant) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <input class="btn" type="submit" value="ELIMINA">
+                        </form>
+                        
+                    </botton>
+                    
+                        
+                    
+                    <botton>
+                        @if($project->trashed())
+                            <form action="{{ route('restaurants.restore',$project) }}" method="POST">
+                                @csrf
+                                <input class="btn btn-sm btn-success" type="submit" value="Ripristina">
+                            </form>
+                        @endif
+                    </botton>
+                
+                @empty 
+                
+                    <p>Vuoto</p>
+                
+                @endforelse
+            </tbody>
+
+        </table>
+
+    </div>
+
+</div>
+
+
+@endsection
---
+++ resources/views/Restaurants/show.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.app')
+
+
+@section('content')
+    
+<div class="container">
+    <div class="d-flex">
+        <div >
+            <h1 class="title">{{$restaurant->company_name}}
+                @if($restaurant->type)
+                <span class="badge rounded-pill bg-warning">{{ $restaurant->typology->compan_name }}</span>
+                @else
+                    <span class="badge rounded-pill bg-secondary">Nessuna tipo trovato</span>
+                @endif
+        </h1></h1>
+            <p class="address">{{$restaurant->slug}}</p>
+            <p class="description">{{$restaurant->description}}</p>
+            <p class="date">{{$restaurant->date}}</p>
+            <p class="url">{{$restaurant->url}}</p>
+            
+        </div>
+        <div class="container">
+            <h2>Articoli correlati</h2>
+            @if($restaurant->type)
+            <ul>
+                @foreach($restaurant->type->restaurants as $type)
+                    <li>
+                        <a href="{{ route('restaurants.show',$type)}}">
+                            {{ $type->title }}
+                        </a>
+                    </li>
+                @endforeach
+            </ul>
+            @else
+                nessun articolo correlato
+            @endif
+        </div>
+        <div>
+            <a class="btn" href="{{route('restaurants.edit',$restaurant)}}">MODIFICA</a>
+            @if($restaurant->trashed())
+                    <form action="{{ route('restaurants.restore',$restaurant) }}" method="POST">
+                        @csrf
+                        <input class="btn btn-sm btn-success" type="submit" value="Ripristina">
+                    </form>
+                @endif
+        </div>
+
+    </div>
+    
+
+</div>
+
+@endsection
