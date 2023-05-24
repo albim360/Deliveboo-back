5bb46919b790101bd43376b62243e4b02d9b09c1
Alberto <74522184+albim360@users.noreply.github.com>
2023-05-24 23:37:07 +0200

    DELETE

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
@@ -8,10 +7,6 @@ use App\Models\Product;
 use App\Models\Typology;
 use App\Http\Requests\StoreProductRequest;
 use App\Http\Requests\UpdateProductRequest;
-
-
-// TODO: controllare le rule
-//use Illuminate\Validation\Rule;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Auth;
 
@@ -26,7 +21,6 @@ class ProductController extends Controller
     {
         $user = Auth::user();
         $products = Product::where('restaurant_id', $user->restaurant->id)->withTrashed()->get();
-        //dd($products);
         return view('products.index', compact('products'));
     }
 
@@ -39,32 +33,30 @@ class ProductController extends Controller
     {
         $typologies = Typology::all();
         $products = Product::where('restaurant_id', Auth::user()->restaurant_id)->get();
-
         return view('products.create', compact('products', 'typologies'));
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Http\Requests\StoreProductRequest  $request
      * @return \Illuminate\Http\Response
      */
     public function store(StoreProductRequest $request)
     {
         $data = $request->validated();
-
         $data['slug'] = Str::slug($data['name']);
         $data['restaurant_id'] = Auth::user()->restaurant->id;
-        //dd(Auth::user()->restaurant->id);
+
         $product = Product::create($data);
 
-        return redirect()->route('products.show', $product);
+        return redirect()->route('products.index')->with('success', 'Il prodotto è stato salvato con successo.');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Models\Product  $product
      * @return \Illuminate\Http\Response
      */
     public function show(Product $product)
@@ -73,14 +65,13 @@ class ProductController extends Controller
             abort(403); // Unauthorized access
         }
 
-
         return view('products.show', compact('product'));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Models\Product  $product
      * @return \Illuminate\Http\Response
      */
     public function edit(Product $product)
@@ -95,13 +86,13 @@ class ProductController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \App\Http\Requests\UpdateProductRequest  $request
+     * @param  \App\Models\Product  $product
      * @return \Illuminate\Http\Response
      */
     public function update(UpdateProductRequest $request, Product $product)
     {
-        if ($product->restaurant_id !== Auth::user()->restaurant->address) {
+        if ($product->restaurant_id !== Auth::user()->restaurant->id) {
             abort(403); // Unauthorized access
         }
 
@@ -109,15 +100,20 @@ class ProductController extends Controller
         $product->update($data);
         $data['slug'] = Str::slug($data['name']);
 
-        return redirect()->route('products.show', $product);
+        return redirect()->route('products.index')->with('success', 'Il prodotto è stato aggiornato con successo.');
     }
 
+    /**
+     * Restore the specified resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
     public function restore(Request $request, Product $product)
     {
-
         if ($product->trashed()) {
             $product->restore();
-
             $request->session()->flash('message', 'Il prodotto è stato ripristinato.');
         }
 
@@ -127,7 +123,7 @@ class ProductController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  \App\Models\Product  $product
      * @return \Illuminate\Http\Response
      */
     public function destroy(Product $product)
@@ -145,5 +141,3 @@ class ProductController extends Controller
         return redirect()->route('products.index');
     }
 }
-
-
--- resources/views/Products/edit.blade.php
+++ resources/views/Products/edit.blade.php
@@ -1,61 +1,50 @@
 @extends('layouts.app')
 
-
 @section('content')
+    <div class="container">
+        <form action="{{ route('products.update', $product) }}" method="POST">
 
+            @csrf
+            @method('PUT')
 
-  <div class="container">
-      <form action="{{ route('products.update',$product)}}" method="POST">
+            <div class="mb-3">
+                <label for="name" class="form-label">Name</label>
+                <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name"
+                    value="{{ old('name', $product->name) }}">
+                @error('name')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
+            </div>
 
-          @csrf
-          @method('PUT')
+            <div class="mb-3">
+                <label for="description" class="form-label">Description</label>
+                <input type="text" class="form-control @error('description') is-invalid @enderror" id="description"
+                    name="description" value="{{ old('description', $product->description) }}">
+                @error('description')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
+            </div>
 
-          <div class="mb-3">
-            <label for="name" class="form-label">name</label>
-            <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" value="{{ old('name',$product->name) }}">
-            @error('name')
-              <div class="invalid-feedback">
-                  {{ $message }}
-              </div>
-            @enderror
-          </div>
+            <div class="mb-3">
+                <label for="price" class="form-label">Price</label>
+                <input type="text" class="form-control @error('price') is-invalid @enderror" id="price" name="price"
+                    value="{{ old('price', $product->price) }}">
+                @error('price')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
+            </div>
 
+            <div class="mb-3">
+                <button type="submit" class="btn btn-primary"><a href="{{ route('products.index') }}" class="btn btn-primary">Salva</a></button>
+                
+            </div>
 
-
-
-          <div class="mb-3">
-            <label for="description" class="form-label">description</label>
-            <input type="text" class="form-control @error('description') is-invalid @enderror" id="description" name="description" value="{{ old('description',$product->description) }}">
-            @error('description')
-              <div class="invalid-feedback">
-                  {{ $message }}
-              </div>
-            @enderror
-
-          </div>
-
-
-
-          <div class="mb-3">
-            <label for="price" class="form-label">price </label>
-            <input type="text" class="form-control @error('price') is-invalid @enderror" id="price" name="price" value="{{ old('price',$product->price) }}">
-            @error('price')
-              <div class="invalid-feedback">
-                  {{ $message }}
-              </div>
-            @enderror
-          </div>
-
-
-          <div class="mb-3">
-
-            <button type="submit" class="btn btn-primary" href="{{ route('products.show', $product) }}">Salva</button>
-          </div>
-
-
-      </form>
-
-
-  </div>
-
+        </form>
+    </div>
 @endsection
