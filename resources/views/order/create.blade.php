7d9e7b77960d55778aa3dd7ab799ddb2b2faf986
Francesco Punta <puntafrancesco@gmail.com>
2023-05-25 13:01:37 +0200

    Order layout in create, show and index

--- resources/views/order/create.blade.php
+++ resources/views/order/create.blade.php
@@ -1,22 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-
 <div class="container">
+    <h1>Nuovo ordine</h1>
+
     <form action="{{ route('orders.store') }}" method="POST">
         @csrf
+
         <div class="mb-3">
-            <label for="full_name" class="form-label">name</label>
-            <input type="text" class="form-control @error('full_name') is-invalid @enderror" id="full_name" name="full_name" value="{{ old('full_name') }}">
+            <label for="full_name" class="form-label">Nome</label>
+            <input type="text" class="form-control @error('full_name') is-invalid @enderror" id="full_name" name="full_name" value="{{ old('full_name') }}" required>
             @error('full_name')
             <div class="invalid-feedback">
                 {{ $message }}
             </div>
             @enderror
         </div>
+
         <div class="mb-3">
-            <label for="email" class="form-label">email</label>
-            <input type="text" class="form-control @error('email') is-invalid @enderror" id="email" name="email" value="{{ old('email') }}">
+            <label for="email" class="form-label">Email</label>
+            <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" name="email" value="{{ old('email') }}" required>
             @error('email')
             <div class="invalid-feedback">
                 {{ $message }}
@@ -25,40 +28,33 @@
         </div>
 
         <div class="mb-3">
-            <label for="telephone" class="form-label">telephone</label>
-            <input type="text" class="form-control @error('telephone') is-invalid @enderror" id="telephone" name="telephone" value="{{ old('telephone') }}">
-            @error('email')
+            <label for="telephone" class="form-label">Telefono</label>
+            <input type="tel" class="form-control @error('telephone') is-invalid @enderror" id="telephone" name="telephone" value="{{ old('telephone') }}" required>
+            @error('telephone')
             <div class="invalid-feedback">
                 {{ $message }}
             </div>
             @enderror
         </div>
 
-        <div class="mb-3"> 
-            <label for="products" class="form-label">Product</label>
-            <div class="d-flex @error('products') is-invalid @enderror flex-wrap gap-3">
-
+        <div class="mb-3">
+            <label for="products" class="form-label">Prodotti</label>
+            <div class="d-flex flex-wrap gap-3">
                 @foreach($products as $key => $product)
-                    <div class="form-check">
-                        <input name="products[]"
-                        @checked( in_array($product->id, old('products',[]) ) )
-                        class="form-check-input"
-                        type="checkbox"
-                        value="{{ $product->id }}" 
-                        id="flexCheckDefault"
-                        >
-                        <label class="form-check-label" for="flexCheckDefault">
-                            {{ $product->name }}
-                        </label>
-                    </div>
+                <div class="form-check">
+                    <input name="products[]" class="form-check-input" type="checkbox" value="{{ $product->id }}" id="product_{{ $product->id }}">
+                    <label class="form-check-label" for="product_{{ $product->id }}">{{ $product->name }}</label>
+                </div>
                 @endforeach
             </div>
         </div>
-        
 
         <div class="mb-3">
-            <label for="total_payment" class="form-label">pagamento</label>
-            <input type="number" class="form-control @error('total_payment') is-invalid @enderror" id="total_payment" name="total_payment" value="{{ old('total_payment') }}">
+            <label for="total_payment" class="form-label">Pagamento</label>
+            <div class="input-group">
+                <span class="input-group-text">€</span>
+                <input type="number" class="form-control @error('total_payment') is-invalid @enderror" id="total_payment" name="total_payment" value="{{ old('total_payment') }}" required>
+            </div>
             @error('total_payment')
             <div class="invalid-feedback">
                 {{ $message }}
@@ -67,8 +63,8 @@
         </div>
 
         <div class="mb-3">
-            <label for="address" class="form-label">address</label>
-            <input type="text" class="form-control @error('address') is-invalid @enderror" id="address" name="address" value="{{ old('address') }}">
+            <label for="address" class="form-label">Indirizzo</label>
+            <input type="text" class="form-control @error('address') is-invalid @enderror" id="address" name="address" value="{{ old('address') }}" required>
             @error('address')
             <div class="invalid-feedback">
                 {{ $message }}
@@ -77,8 +73,8 @@
         </div>
 
         <div class="mb-3">
-            <label for="date" class="form-label">date</label>
-            <input type="date" class="form-control @error('date') is-invalid @enderror" id="date" name="date" value="{{ old('date') }}">
+            <label for="date" class="form-label">Data</label>
+            <input type="date" class="form-control @error('date') is-invalid @enderror" id="date" name="date" value="{{ old('date') }}" required>
             @error('date')
             <div class="invalid-feedback">
                 {{ $message }}
@@ -89,5 +85,13 @@
         <button type="submit" class="btn btn-primary">Salva</button>
     </form>
 </div>
-
 @endsection
+
+
+<style>
+.form-label {
+    font-weight: bold;
+    font-size: 16px;
+}
+
+</style>
--- resources/views/order/show.blade.php
+++ resources/views/order/show.blade.php
@@ -1,47 +1,61 @@
 @extends('layouts.app')
 
 @section('content')
-@if(request()->session()->exists('message'))
-
-<div class="alert alert-primary" role="alert">
-    {{ request()->session()->pull('message') }}
-</div>
+@if (request()->session()->exists('message'))
+    <div class="alert alert-primary" role="alert">
+        {{ request()->session()->pull('message') }}
+    </div>
 @endif
+
 <div class="container py-5">
-        <div class="d-flex align-items-center">
-            <div class="me-auto">
-                <h1>{{ $order->full_name }}</h1>
-                
-                <ul class="ps-0 d-flex gap-1">
-                @forelse($order->products as $product )
+    <div class="d-flex align-items-center">
+        <div class="me-auto">
+            <h1>{{ $order->full_name }}</h1>
+            <ul class="ps-0 d-flex gap-1">
+                @forelse ($order->products as $product)
                     <span class="badge rounded-pill text-bg-light">{{ $product->name }}</span>
                 @empty
-                    -
+                    <span class="badge rounded-pill text-bg-light">-</span>
                 @endforelse
-                </ul>
-            </div>
+            </ul>
         </div>
     </div>
-    <div class="container">
-        <div>
-            <h5>date: </h5>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <h5>Data:</h5>
             <p>{{ $order->date }}</p>
         </div>
-        <div>
-            <h5>address: </h5>
-            <p>{{ $order->address }}</p>
-        </div>
-        <div>
-            <h5>pagamento: </h5>
+        <div class="col-md-6">
+            <h5>Pagamento:</h5>
             <p>{{ $order->total_payment }}</p>
         </div>
-        <div>
-            <h5>telephone: </h5>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <h5>Indirizzo:</h5>
+            <p>{{ $order->address }}</p>
+        </div>
+        <div class="col-md-6">
+            <h5>Telefono:</h5>
             <p>{{ $order->telephone }}</p>
         </div>
-        <div>
-            <h5>email: </h5>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <h5>Email:</h5>
             <p>{{ $order->email }}</p>
         </div>
+        <div class="col-md-6">
+            <h5>Descrizione:</h5>
+            <p>{{ $order->description }}</p>
+        </div>
     </div>
+    
+    <div class="mt-4">
+        <a href="{{ route('orders.index') }}" class="btn btn-secondary">Torna a tutti gli ordini</a>
+    </div>
+</div>
 @endsection
