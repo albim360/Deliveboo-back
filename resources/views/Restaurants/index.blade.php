e8dd50b265fba21e6925392b72adbbd7f95a7212
Pietro Maione <pietromaione9@gmail.com>
2023-05-26 11:44:37 +0200

    FIX img register

--- resources/views/Restaurants/index.blade.php
+++ resources/views/Restaurants/index.blade.php
@@ -1,57 +1,50 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div>
-        <h1>
-        RISTORANTI
-    </h1>
+    <div class="container">
         <div>
+            <h1>
+                RISTORANTI
+            </h1>
+            <div>
 
+            </div>
         </div>
-    </div>
 
-                {{-- @dd($restaurants); --}}
-                @forelse ($restaurants as $restaurant)
+        {{-- @dd($restaurants); --}}
+        @forelse ($restaurants as $restaurant)
+            {{-- @dd($restaurants); --}}
+            <img src="{{ asset('storage/' . $restaurant->img_way) }}" alt="">
+            <p>{{ $restaurant->id }}</p>
+            <p><a href="{{ route('restaurants.show', $restaurant) }}">{{ $restaurant->company_name }}</a></p>
+            <p>{{ $restaurant->address }}</p>
+            <p>{{ $restaurant->description }}</p>
+            <p>{{ $restaurant->vat_number }}</p>
+            <p>{{ $restaurant->telephone }}</p>
 
-                    {{-- @dd($restaurants); --}}
-                    <p>{{$restaurant->id}}</p>
-                    <p><a href="{{route('restaurants.show',$restaurant)}}">{{$restaurant->company_name}}</a></p>
-                    <p>{{$restaurant->address}}</p>
-                    <p>{{$restaurant->description}}</p>
-                    <p>{{$restaurant->vat_number}}</p>
-                    <p>{{$restaurant->telephone}}</p>
+            <p>
+                {{-- {{ $restaurant->trashed() ? $restaurant->deleted_at : '' }} --}}
+            </p>
 
-                    <p>
-                        {{-- {{ $restaurant->trashed() ? $restaurant->deleted_at : '' }} --}}
-                    </p>
-
-                    <p>
-                        @forelse ($restaurant->typologies as $typology)
-                        <span>{{$typology->category_kitchen}}</span>
-                        @empty
-                        <span>-</span>
-                        @endforelse
-                    </p>
-
-
-
-
-
-
-
+            <p>
+                @forelse ($restaurant->typologies as $typology)
+                    <span>{{ $typology->category_kitchen }}</span>
                 @empty
-
-                    <p>Vuoto</p>
-
+                    <span>-</span>
                 @endforelse
-            </tbody>
+            </p>
+
+        @empty
+
+            <p>Vuoto</p>
+        @endforelse
+        </tbody>
 
         </table>
 
     </div>
 
-</div>
+    </div>
 
 
 @endsection
